4496d6407e9acf98c2a5b920a7e7fc4602d03dd6
Michael Weiss <michael.weiss@m10s.at>
2016-09-15 20:39:06 +0200

    Add Architecture Skeleton

--- src/app/Main.elm
+++ src/app/Main.elm
@@ -1,11 +1,63 @@
 module Main exposing (..)
 
 import Html exposing (..)
+import Html.App exposing (program)
+
+
+-- MODEL
+
+
+type alias Model =
+    String
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( "Hello World!", Cmd.none )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    text model
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
 
 
 -- MAIN
 
 
-main : Html ()
+main : Program Never
 main =
-    text "It works!"
+    program
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
